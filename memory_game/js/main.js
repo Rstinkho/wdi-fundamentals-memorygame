f98376d151485260373abf46e6effc25d35acdb5
Taras <rstinkho@bk.ru>
2018-08-21 14:28:22 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "queenOne";
+var cardTwo = "queenTwo";
+var cardThree = "kingOne";
+var cardFour ="kingFour";
+console.log("Player flipped " + cardOne);
+console.log("Player flipped " + cardTwo);
